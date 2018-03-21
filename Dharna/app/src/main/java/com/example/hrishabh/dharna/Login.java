5ddc5019c1fe58cff44a38ca58fe9d1d5c3fbc24
Your Name <hrishabhprakash@gmail.com>
2018-03-21 18:21:28 +0530

    Commit message

--- Dharna/app/src/main/java/com/example/hrishabh/dharna/Login.java
+++ Dharna/app/src/main/java/com/example/hrishabh/dharna/Login.java
@@ -1,11 +1,19 @@
 package com.example.hrishabh.dharna;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
@@ -13,22 +21,48 @@ public class Login extends AppCompatActivity {
     private Button login;
     private EditText luser,lpass;
     FirebaseAuth lauth;
-
+    ProgressDialog lprogressDialog;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        lprogressDialog=new ProgressDialog(this);
         luser=(EditText) findViewById(R.id.login_email);
         lpass=(EditText) findViewById(R.id.login_pass);
         login=(Button) findViewById(R.id.login);
         lauth=FirebaseAuth.getInstance();
+        textView=(TextView) findViewById(R.id.lrefer);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lauth.signInWithEmailAndPassword()
+                if(TextUtils.isEmpty(luser.getText().toString().trim())){
+                    Toast.makeText(Login.this,"Enter Email",Toast.LENGTH_SHORT);
+                    return;
+                } else if(TextUtils.isEmpty(lpass.getText().toString().trim())){
+                    Toast.makeText(Login.this,"Enter Pass",Toast.LENGTH_SHORT);
+                    return;
+                } else{
+                    lprogressDialog.setMessage("Logging in");
+                    lprogressDialog.show();
+                    lauth.signInWithEmailAndPassword(luser.getText().toString().trim(),lpass.getText().toString().trim()).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Login.this,"Logged in",Toast.LENGTH_SHORT).show();
+                                lprogressDialog.dismiss();
+                            } else{
+                                Toast.makeText(Login.this,"Login fail, try again",Toast.LENGTH_SHORT).show();
+                                lprogressDialog.dismiss();
+                                return;
+                            }
+
+                        }
+                    });
+                }
             }
         });
 
--- Dharna/app/src/main/java/com/example/hrishabh/dharna/Signup.java
+++ Dharna/app/src/main/java/com/example/hrishabh/dharna/Signup.java
@@ -1,6 +1,7 @@
 package com.example.hrishabh.dharna;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,6 +25,7 @@ public class Signup extends AppCompatActivity {
     private Button button;
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
+    private TextView textView;
 
 
     @Override
@@ -36,6 +39,16 @@ public class Signup extends AppCompatActivity {
         pass=(EditText) findViewById(R.id.pass);
         cpass=(EditText) findViewById(R.id.cpass);
         button=(Button) findViewById(R.id.register);
+        textView=(TextView) findViewById(R.id.refer);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Signup.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,16 +69,22 @@ public class Signup extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(Signup.this,"User Registered",Toast.LENGTH_LONG).show();
+                                    Toast.makeText(Signup.this,"User Registered",Toast.LENGTH_SHORT).show();
+                                    progressDialog.dismiss();
+                                    Intent intent=new Intent(Signup.this,Login.class);
+                                    startActivity(intent);
                                 }
                                 else{
-                                    Toast.makeText(Signup.this,"Could not registered",Toast.LENGTH_LONG).show();
+                                    Toast.makeText(Signup.this,"Could not registered",Toast.LENGTH_SHORT).show();
+                                    progressDialog.dismiss();
+                                    return;
                                 }
                             }
                         });
                     }
                 }else {
                     Toast.makeText(Signup.this, "Password not matched", Toast.LENGTH_SHORT).show();
+                    return;
                 }
             }
         });
