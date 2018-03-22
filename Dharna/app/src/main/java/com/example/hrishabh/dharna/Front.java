c242e7fd737f67fdc38e434fbc51faa3612df08f
Your Name <hrishabhprakash@gmail.com>
2018-03-22 16:41:10 +0530

    Commit message

--- Dharna/app/src/main/java/com/example/hrishabh/dharna/Front.java
+++ Dharna/app/src/main/java/com/example/hrishabh/dharna/Front.java
@@ -1,13 +1,29 @@
 package com.example.hrishabh.dharna;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Front extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+
+        button=(Button) findViewById(R.id.test);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Front.this,Signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
